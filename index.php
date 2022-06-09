624b2653d148afac247de403f363c2cf4acebc95
Abdelrahman Ibrahim <abdelramin30@gmail.com>
2022-06-09 03:01:28 +0200

    First Weaak 1_5

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+/* Assignments Link :- https://elzero.org/php-bootcamp-assignments-lesson-from-1-to-5/
+*/
+# Task 1
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= " My First PHP Page"?></title>
+  </head>
+  <body>
+    <div><?php echo "*** TASK 1 ***"."<br>";?></div>
+    <div><?php echo "We Love"."<br>";?></div>
+    <div><?php print "Elzero Channel"."<hr>"?></div>
+  </body>
+</html>
+
+<!-- ############################################# -->
+<?php 
+// Task 2
+/*
+echo 'Prevent Me From Running Please';
+echo 'Prevent Me From Running Please';
+echo 'Prevent Me From Running Please';
+*/
+?>
+
+<!-- ############################################# -->
+<?php 
+// Task 3
+/*
+My Application
+Version 1.0
+Created By Elzero
+*/
+?>
+
+<!-- ############################################# -->
+<?php 
+// Task 4
+// ## First Comment
+# // # Second Comment
+/* /* /* Third Comment */
+////// Fourth Comment
+?>
